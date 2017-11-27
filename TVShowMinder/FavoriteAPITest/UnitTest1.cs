439a344128af33c690586a8831c16316fb4e1560
Jan Jalinski <jalinski@outlook.com>
2017-11-27 13:29:58 +0100

    init commit

---
+++ TVShowMinder/FavoriteAPITest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace FavoriteAPITest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TVShowMinder/MovieDBAPITest/MovieDBTest.cs
@@ -0,0 +1,21 @@
+namespace MovieDBAPITest
+{
+    using System;
+
+    using Xunit;
+
+    /// <summary>
+    /// The movie db test.
+    /// </summary>
+    public class MovieDBTest
+    {
+        /// <summary>
+        /// The get connection to movie dbapi.
+        /// </summary>
+        [Fact]
+        public void GetConnectionToMovieDBAPI()
+        {
+
+        }
+    }
+}
